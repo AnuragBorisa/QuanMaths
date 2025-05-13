8921af23e2fd8cf06b84afd9c115eb793cc09031
Anurag Borisa <anuragborisa@gmail.com>
2025-05-13 17:38:35 +0530

    orthogonal and normalisation

--- Quantum Computing Basics (The Essentials)/Single-Qubit Intuition/Thecomputationalbasis.py
+++ Quantum Computing Basics (The Essentials)/Single-Qubit Intuition/Thecomputationalbasis.py
@@ -11,3 +11,19 @@ print("⟨0|0⟩ =",np.vdot(zero,zero))
 print("⟨1|1⟩ =",np.vdot(one,one))
 print("⟨0|1⟩ =",np.vdot(zero,one))
 print("⟨1|0⟩ =",np.vdot(one,zero))
+
+# Compute ⟨ψ∣ψ⟩.
+# ∣ψ⟩ =  1/root2 |0⟩ + 1/root2 |1⟩ ,
+
+psi = (zero + one) / np.sqrt(2)
+
+norm = np.vdot(psi,psi)
+
+print("⟨ψ|ψ⟩ =", norm)
+
+phi = (1j*zero + one) / np.sqrt(2)
+
+norm2 = np.vdot(phi,phi)
+
+print("⟨ψ|ψ⟩ =", norm2)
+
